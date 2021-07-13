67e759e687c6f1a74bc165eb39da87f941436c98
Steve Mandl <sjm34@cornell.edu>
2021-07-13 16:58:45 -0400

    packaging stuff

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(HERE / "README.md", "r") as fh:
 
 setuptools.setup(
     name="redismq-smandl",
-    version="0.0.1",
+    version="0.0.1rc1",
     author="Steve Mandl",
     author_email="sjm34@cornell.edu",
     description="Message Queueing for Redis Streams",
@@ -20,7 +20,15 @@ setuptools.setup(
     url="https://github.com/pypa/redismq",
     packages=setuptools.find_packages(exclude=("tests",)),
     install_requires=['aioredis'],
+    extras_require={
+        'dev': ['pytest', 'pytest-asyncio', 'pytest-cov', 'pylint']
+    },
     classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
