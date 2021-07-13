7f545e73dbd39b8912c51b6abe49e1e182721d5c
Steve Mandl <sjm34@cornell.edu>
2021-07-13 17:24:06 -0400

    removed EMCS redis shell scripts

--- emcs-redis-aws.sh
+++
@@ -1,3 +0,0 @@
-export EMCS_REDIS_HOST=mq.emcs.cucloud.net
-export EMCS_REDIS_PORT=6379
-export EMCS_REDIS_DB=0
--- emcs-redis-swarm.sh
+++
@@ -1,3 +0,0 @@
-export EMCS_REDIS_HOST=swarm-1.emcs.cornell.edu
-export EMCS_REDIS_PORT=6379
-export EMCS_REDIS_DB=0
