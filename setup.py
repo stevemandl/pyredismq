1a6a25240aedc802459831099b8c8de8a5e6de0e
Steve Mandl <sjm34@cornell.edu>
2021-07-19 13:21:26 -0400

    release 1.0.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(HERE / "README.md", "r") as fh:
 
 setuptools.setup(
     name="redismq",
-    version="1.0.2",
+    version="1.0.3",
     author="Steve Mandl",
     author_email="sjm34@cornell.edu",
     description="Message Queueing for Redis Streams",
