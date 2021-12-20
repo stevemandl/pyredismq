71fe7a1d26f9aa64b3471c8a87c48c4fd2fa46b3
Joel Bender <joel@betelgeuse>
2021-12-20 09:32:24 -0500

    extract version info from package

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 # setup.py
 
+import re
 import pathlib
 import setuptools
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
 
+# load in the project long description
 with open(HERE / "README.md", "r") as fh:
     long_description = fh.read()
 
+# load in the project metadata
+init_py = open(HERE / "redismq" / "__init__.py").read()
+metadata = dict(re.findall('__([a-z]+)__ = "([^"]+)"', init_py))
+
 setuptools.setup(
     name="redismq",
-    version="1.0.7",
+    version=metadata["version"],
     author="Steve Mandl",
     author_email="sjm34@cornell.edu",
     description="Message Queueing for Redis Streams",
@@ -19,9 +25,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/stevemandl/pyredismq",
     packages=setuptools.find_packages(exclude=("tests",)),
-    install_requires=['aioredis'],
+    install_requires=["aioredis"],
     extras_require={
-        'dev': ['pytest', 'pytest-asyncio', 'pytest-cov', 'pytest-timeout', 'pylint']
+        "dev": ["pytest", "pytest-asyncio", "pytest-cov", "pytest-timeout", "pylint"]
     },
     classifiers=[
         # How mature is this project? Common values are
